5e86751674fd0a2756b2475a452e5f9e42e7c426
FAVOUR <truelyfavoured@gmail.com>
2018-09-13 12:03:09 +0100

    week-one-assignment on class component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import react, {Component } from 'react'
+
+
+
+class App extends Component{
+
+	render(){
+
+		return
+		<div>
+		  
+            <h1>Holla at my React Project</h1>
+		</div>
+	}
+
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import react, { Component }from 'react'
+import {render } from 'react-dom'
+import {App} from "App"
+
+render( <App />, document.getElementById('root'))
